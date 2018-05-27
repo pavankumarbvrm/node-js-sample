2456a83db3716ccba789dad436cff1c144d6a786
pavankumarbvrm <pavankumarbvrm@gmail.com>
2018-05-27 10:42:20 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cd /home/ubuntu/deployment
+cd /home/ubuntu/
+sudo git clone https://github.com/pavankumarbvrm/node-js-sample.git
+cd /home/ubuntu/node-js-sample
 sudo npm install
 sudo npm start
