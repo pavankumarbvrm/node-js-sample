af0280e2b6a75b90c1e9db61d749251e52730a6e
pavankumarbvrm <pavankumarbvrm@gmail.com>
2021-11-29 14:39:22 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd /home/ubuntu/
-sudo git clone https://github.com/pavankumarbvrm/node-js-sample.git
-cd /home/ubuntu/node-js-sample
+echo "Some text here." > /var/www/html/index.nginx-debian.html
+# cd /home/ubuntu/
+# sudo git clone https://github.com/pavankumarbvrm/node-js-sample.git
+# cd /home/ubuntu/node-js-sample
 #sudo npm config set save false
 #sudo npm install
 #sudo npm start
