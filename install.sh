6c8ec1fdd7ef6138a61c6302f54574cc4370d01c
pavankumarbvrm <pavankumarbvrm@gmail.com>
2021-11-29 14:29:13 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 cd /home/ubuntu/
 sudo git clone https://github.com/pavankumarbvrm/node-js-sample.git
 cd /home/ubuntu/node-js-sample
-sudo npm config set save false
-sudo npm install
-sudo npm start
+#sudo npm config set save false
+#sudo npm install
+#sudo npm start
